b5240dbf4bb438d5f35c488921de71385748152c
GaTrani <gabrieltrani02@gmail.com>
2022-11-29 14:24:45 -0300

    Update GerenciarProduto.java

--- Gerenciar/src/GerenciarProduto.java
+++ Gerenciar/src/GerenciarProduto.java
@@ -22,18 +22,36 @@ public class GerenciarProduto {
             op = Integer.parseInt(sc.nextLine());
             switch(op){
                 case 1:
+                    Fornecedor f1 = new Fornecedor();
+                    System.out.println("Digite o Codigo do fornecedor: ");
+                    int codigo = Integer.parseInt(sc.nextLine());
+                    f1.setCodigo(codigo);
+                    System.out.println("Digite a Razao Social do fornecedor: ");
+                    String rs =  sc.nextLine();
+                    f1.setRazaoSocial(rs);
+                    System.out.println("Digite o Enderedo do fornecedor: ");
+                    String endereco =  sc.nextLine();
+                    f1.setEndereco(endereco);
+                    System.out.println("Digite a Cidade do fornecedor: ");
+                    String cidade =  sc.nextLine();
+                    f1.setCidade(cidade);
                     System.out.println("Digite o Estado do fornecedor [UF]: ");
                     String uf =  sc.nextLine();
-                    Fornecedor f1 = new Fornecedor();
                     f1.setEstado(uf);
+                    //Contato
+                    Contato c1 = new Contato();
+                    System.out.println("Digite o Nome Contato do fornecedor: ");
+                    String contato =  sc.nextLine();
+                    c1.setContato(contato);
+                    System.out.println("Digite o Telefone do fornecedor: ");
+                    String telefone =  sc.nextLine();
+                    c1.setTelefone(telefone);
+                    System.out.println("Digite o Email do fornecedor: ");
+                    String email = sc.nextLine();
+                    c1.setEmail(email);
+                    f1.setContato(c1);
                     Fornecedor.fornecedores.add(f1);
 
-                    //codigo;
-                    //razaoSocial;
-                    //endereco;
-                    //cidade;
-                    //estado;
-                    //contato;
                     f1.listarFornecedores();
                     break;
                 case 2:
